7adb3093a4d7670194926bb8315c03b39e2d5d5c
KiaSkretteberg <kiaskretteberg@gmail.com>
2023-01-30 20:26:50 -0700

    Creating basic Microchip Studio Project

---
+++ ArvenSensorControls/main.c
@@ -0,0 +1,18 @@
+/*
+ * ArvenSensorControls.c
+ *
+ * Created: 2023-01-30 8:22:18 PM
+ * Author : Kya
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
