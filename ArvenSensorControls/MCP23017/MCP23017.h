1276a57401a03cca7712a96298bc5706ba215945
nmanhas1 <nmanhas1@nait.ca>
2023-03-10 12:38:45 -0700

    .

--- ArvenSensorControls/MCP23017/MCP23017.h
+++ ArvenSensorControls/MCP23017/MCP23017.h
@@ -17,8 +17,8 @@
 * Author: Nubal Manhas
 */
 
-/* I2C address for MCP23017
-* our I2C library adjusts it
+/* I2C address for MCP23017.
+* Our I2C library adjusts it
 * for 7 bit addressing, so no
 * need to account for that.
 * This is assuming that A0, A1, and A2 are 
@@ -37,9 +37,9 @@
 */
 typedef enum
 {
-	INPUT = 1,
-	OUTPUT = 0
-} PinMode;
+	MCP23017_PinMode_INPUT = 1,
+	MCP23017_PinMode_OUTPUT = 0
+} MCP23017_PinMode;
 
 /*
 * According to the datasheet:
@@ -67,33 +67,23 @@ typedef enum
 */
 typedef enum
 {
-	PIN0_ADDR = 0b00000001,
-	PIN1_ADDR = 0b00000010,
-	PIN2_ADDR = 0b00000100,
-	PIN3_ADDR = 0b00001000,
-	PIN4_ADDR = 0b00010000,
-	PIN5_ADDR = 0b00100000,
-	PIN6_ADDR = 0b01000000,
-	PIN7_ADDR = 0b10000000,
-} MCP23017_PINADDR;
+	MCP23017_BIT0_ADDR = 0b00000001,
+	MCP23017_BIT1_ADDR = 0b00000010,
+	MCP23017_BIT2_ADDR = 0b00000100,
+	MCP23017_BIT3_ADDR = 0b00001000,
+	MCP23017_BIT4_ADDR = 0b00010000,
+	MCP23017_BIT5_ADDR = 0b00100000,
+	MCP23017_BIT6_ADDR = 0b01000000,
+	MCP23017_BIT7_ADDR = 0b10000000,
+} MCP23017_BITADDR;
+
 typedef enum
 {
-		HIGH = 1,
-		LOW = 0
+		MCP23017_OUTPUT_HIGH = 1,
+		MCP23017_OUTPUT_LOW = 0
 }MCP23017_OUTPUT;
-typedef enum
-{
-	PIN0_NUM = 0,
-	PIN1_NUM = 1,
-	PIN2_NUM = 2,
-	PIN3_NUM = 3,
-	PIN4_NUM = 4,
-	PIN5_NUM = 5,
-	PIN6_NUM = 6,
-	PIN7_NUM = 7,
-		
-}MCP23017_PINNUM;
+
 void MCP23017_Init(MCP23017_PORT port);
-void MCP23017_Send(MCP23017_OUTPUT output, MCP23017_PORT port, MCP23017_PINADDR pin);
-char MCP23017_ReadPin(MCP23017_PORT port, MCP23017_PINADDR pin);
-void MCP23017_SetPin(PinMode mode, MCP23017_PORT port, MCP23017_PINADDR pin);
+void MCP23017_Send(MCP23017_OUTPUT output, MCP23017_PORT port, MCP23017_BITADDR pin);
+char MCP23017_ReadPin(MCP23017_PORT port, MCP23017_BITADDR pin);
+void MCP23017_SetPin(MCP23017_PinMode mode, MCP23017_PORT port, MCP23017_BITADDR pin);
